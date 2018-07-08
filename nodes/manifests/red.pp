1f31898252a3e0a8a7ac4832a0c112cb6695b94c
Chris Do <root@puppet.skull.local>
2018-07-08 12:04:32 +1000

    Adjust cron run times

--- nodes/manifests/red.pp
+++ nodes/manifests/red.pp
@@ -28,13 +28,13 @@ class nodes::red {
   crontab::cron { "root":
     cronjobs => [ 
 
-      "15 5 * * * /usr/bin/rsync -avz --delete --exclude='repo*' rsync://centos.melbourneitmirror.net/centos/7/os/x86_64/ /home/red/repos/centos/7/os/x86_64/",
-      "30 6 * * * /usr/bin/rsync -avz --delete --exclude='repo*' rsync://centos.melbourneitmirror.net/centos/7/updates/x86_64/ /home/red/repos/centos/7/updates/x86_64/",
-      "45 8 * * * /usr/bin/rsync -avz --delete --exclude='repo*' --exclude='debug' rsync://mirrors.rit.edu/epel/7/x86_64/ /home/red/repos/epel/7/x86_64/",
+      "15 3 * * * /usr/bin/rsync -avz --delete --exclude='repo*' rsync://centos.melbourneitmirror.net/centos/7/os/x86_64/ /home/red/repos/centos/7/os/x86_64/",
+      "30 4 * * * /usr/bin/rsync -avz --delete --exclude='repo*' rsync://centos.melbourneitmirror.net/centos/7/updates/x86_64/ /home/red/repos/centos/7/updates/x86_64/",
+      "45 5 * * * /usr/bin/rsync -avz --delete --exclude='repo*' --exclude='debug' rsync://mirrors.rit.edu/epel/7/x86_64/ /home/red/repos/epel/7/x86_64/",
 
-      "0 9 * * *  /usr/bin/createrepo --update /home/red/repos/centos/7/os/x86_64/",
-      "15 9 * * * /usr/bin/createrepo --update /home/red/repos/centos/7/updates/x86_64/",
-      "30 9 * * * /usr/bin/createrepo --update /home/red/repos/epel/7/x86_64/",
+      "0 6 * * *  /usr/bin/createrepo --update /home/red/repos/centos/7/os/x86_64/",
+      "15 6 * * * /usr/bin/createrepo --update /home/red/repos/centos/7/updates/x86_64/",
+      "30 6 * * * /usr/bin/createrepo --update /home/red/repos/epel/7/x86_64/",
 
     ],  
   }
