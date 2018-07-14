1ed19ab6a407360a76bc6031f5a9dbf5f87c139b
Chris Do <root@puppet.skull.local>
2018-07-14 23:53:48 +1000

    Remove params file

--- remiphp/manifests/params.pp
+++
@@ -1,9 +0,0 @@
-class remiphp::params {
-
-  case $::osfamily {
-    'RedHat', 'CentOS':  { $package_php = hiera('remiphp::redhat_php') }
-    /^(Debian|Ubuntu)$/: { $package_php = hiera('remiphp::debian_php')  }
-    default:             { warning ('Operating System not supported') }
-  }
-
-}
